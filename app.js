2afcc06889b77ed7a5eb9ae63484b56341f1bbbd
Sri Krishna <100750998+marvelsri@users.noreply.github.com>
2022-12-08 09:26:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function booked(){
+    alert("Your Ticket Has Been Successfully Booked.")
+    window.location.reload();
+}
+
+function submitted(){
+    alert("Your Message Has Been Sent.")
+    window.location.reload();
+}
+
+
